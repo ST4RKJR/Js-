9ab6709672203e3869581a1bdb3319b9676a71b8
Stark <sahil.gen9@gmail.com>
2025-01-20 23:34:15 +0530

    Find Pair with Target Sum - Hard version.py

---
+++ Assignment/Find Pair with Target Sum - Hard version.py
@@ -0,0 +1,11 @@
+def has_pair_with_sum(arr, target):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target:
+            return True
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return False
