579bda0cba495a85556c5b56134a02857c179f53
Stark <sahil.gen9@gmail.com>
2025-01-22 15:07:01 +0530

    lab4.js

--- LABS/lab4.js
+++ LABS/lab4.js
@@ -14,3 +14,33 @@ function accessCallback(isAuthorized) {
     }
 }
 
+//Museum Ticket Booking
+
+function calculateTicketPrice(age, isWeekend, hasStudentCard) {
+    let price = 12;
+
+    if (age < 12) {
+        price -= 5;
+    }
+    if (isWeekend) {
+        price += 3;
+    }
+    if (hasStudentCard) {
+        price -= 2;
+    }
+
+    return price;
+}
+
+//Calculator Operation
+
+function doubleResult(func) {
+    return function(x) {
+      return 2 * func(x);
+    };
+  }
+  
+  function addFive(x) {
+    return x + 5;
+  }
+
