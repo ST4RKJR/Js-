751522a4a0fc3e0806de7629b1e72365d2a5a59c
Stark <sahil.gen9@gmail.com>
2025-01-24 09:49:36 +0530

    lab4.js

--- LABS/lab4.js
+++ LABS/lab4.js
@@ -1,65 +1,79 @@
-//JS-Authentication System
+// //JS-Authentication System
 
 
-function authenticateUser(username, callback, users) {
-    const userExists = users.includes(username);
-    callback(userExists);
-}
+// function authenticateUser(username, callback, users) {
+//     const userExists = users.includes(username);
+//     callback(userExists);
+// }
 
-function accessCallback(isAuthorized) {
-    if (isAuthorized) {
-        console.log("Access granted");
-    } else {
-        console.log("Access denied");
-    }
-}
+// function accessCallback(isAuthorized) {
+//     if (isAuthorized) {
+//         console.log("Access granted");
+//     } else {
+//         console.log("Access denied");
+//     }
+// }
 
-//Museum Ticket Booking
+// //Museum Ticket Booking
 
-function calculateTicketPrice(age, isWeekend, hasStudentCard) {
-    let price = 12;
+// function calculateTicketPrice(age, isWeekend, hasStudentCard) {
+//     let price = 12;
 
-    if (age < 12) {
-        price -= 5;
-    }
-    if (isWeekend) {
-        price += 3;
-    }
-    if (hasStudentCard) {
-        price -= 2;
-    }
+//     if (age < 12) {
+//         price -= 5;
+//     }
+//     if (isWeekend) {
+//         price += 3;
+//     }
+//     if (hasStudentCard) {
+//         price -= 2;
+//     }
 
-    return price;
-}
+//     return price;
+// }
 
-//Calculator Operation
+// //Calculator Operation
 
-function doubleResult(func) {
-    return function(x) {
-      return 2 * func(x);
-    };
-  }
+// function doubleResult(func) {
+//     return function(x) {
+//       return 2 * func(x);
+//     };
+//   }
   
-  function addFive(x) {
-    return x + 5;
-  }
+//   function addFive(x) {
+//     return x + 5;
+//   }
 
-//Debugging Multiplication 
+// //Debugging Multiplication 
 
-function calculateProduct(...numbers) {
-    let product = Number(1); 
-    let count =0
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i]!=0){
-        product *= (numbers[i]);
-        }
-        else{
-            count +=1
-        }
-    }
-    if (count>0){
-        return product;}
-    else{
-        return 1
-    }
-}
+// function calculateProduct(...numbers) {
+//     let product = Number(1); 
+//     let count =0
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i]!=0){
+//         product *= (numbers[i]);
+//         }
+//         else{
+//             count +=1
+//         }
+//     }
+//     if (count>0){
+//         return product;}
+//     else{
+//         return 1
+//     }
+// }
+// function greet(name = "Vishal Sir") {  
+//     return `Good Morning, ${name}!`;  
+//   }  
+  
+//   console.log(greet(undefined));
+
+// var calculate = function(x, y, z = 8) {
+//     return x * y / z + z;
+// };
+
+// let ans = calculate(2, 6, 3);
+// console.log(ans);
+
+
