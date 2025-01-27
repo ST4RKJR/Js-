0b2f80be128ce928e5c99268234dec735d6c0044
Stark <sahil.gen9@gmail.com>
2025-01-27 09:41:27 +0530

    Last_occurence.py

--- Assignment/Last_occurence.py
+++ Assignment/Last_occurence.py
@@ -7,18 +7,26 @@ ans = -1
 # rev.sort()
 
 
-for i in range(len(s)-1,-1,-1):
-    if s[i] == c:
-        ans = i
-        break
-print(ans)
+# for i in range(len(s)-1,-1,-1):
+#     if s[i] == c:
+#         ans = i
+#         break
+# print(ans)
 
-maxi = 0
+# maxi = 0
+# for i in range(len(s)):
+#     if s[i] == c:
+#         maxi = i 
+        
+# if maxi == 0:
+#     print(-1)
+# else:
+#     print(maxi)
+    
+    
+maxi = -1
 for i in range(len(s)):
     if s[i] == c:
         maxi = i 
         
-if maxi == 0:
-    print(-1)
-else:
-    print(maxi)
+print(maxi)
