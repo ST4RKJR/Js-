b4f89edca146c799d7766defaedb9cad4e4f4a39
Stark <sahil.gen9@gmail.com>
2025-01-27 09:40:14 +0530

    Last_occurence.py

--- Assignment/Last_occurence.py
+++ Assignment/Last_occurence.py
@@ -12,3 +12,13 @@ for i in range(len(s)-1,-1,-1):
         ans = i
         break
 print(ans)
+
+maxi = 0
+for i in range(len(s)):
+    if s[i] == c:
+        maxi = i 
+        
+if maxi == 0:
+    print(-1)
+else:
+    print(maxi)
