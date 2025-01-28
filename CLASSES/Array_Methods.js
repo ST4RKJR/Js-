2bff3dc6eef75c7e0bf00f8d20ccf87b04c8345f
Stark <sahil.gen9@gmail.com>
2025-01-28 09:32:32 +0530

    Array_Methods.js

--- CLASSES/Array_Methods.js
+++ CLASSES/Array_Methods.js
@@ -1,13 +1,45 @@
 let array = [1,2,3,4,5]
 // let x = 4
-for (let i = 0;i < array.length; i++){
-    if (array[i] == x){
-        console.log(array[i]**3)
-    }else{
-        console.log("NOT FOUND")
-    }
+// for (let i = 0;i < array.length; i++){
+//     if (array[i] == x){
+//         console.log(array[i]**3)
+//     }else{
+//         console.log("NOT FOUND")
+//     }
+// }
+
+// for (let num of array){
+//     console.log(num**3)
+// }
+
+let printCube = (x) => (console.log(x**3))
+for (let i of array){
+    printCube(i)
 }
 
-for (let num of array){
+array.forEach(printCube)
+
+array.forEach(function (x){
+    console.log(x**3)
+})
+
+array.forEach(num => {
     console.log(num**3)
-}
+})
+
+array.forEach(num => console.log(num**3))
+
+
+let hari = [2,3,4,5,6,7]
+
+let s = hari.map(marks => { //does not returns the value for each iteration when {} used or not return used
+     marks*2
+})
+let t = hari.map(marks => marks*2) //returns the square for every value and the map gives the array of those elements have the same length 
+
+console.log(s)
+console.log(t)
+
+
+
+
