6920bcd5ff974f062d8f7518a047475691d876e3
Stark <sahil.gen9@gmail.com>
2025-01-30 13:05:27 +0530

    Search in 2D Matrix.py

--- Assignment/Search in 2D Matrix.py
+++ Assignment/Search in 2D Matrix.py
@@ -36,3 +36,33 @@ def searchMatrix(mat, target):
 
 
     
+
+def binary_search_on_row(mat,target,row):
+    M = len(mat[0])
+    left = 0 
+    right = M - 1
+    while left <= right:
+        middle = (left+ right)//2
+        if mat[row][middle] == target:
+            return True
+        elif mat[row][middle] > target:
+            right = middle - 1
+        else:
+            left = mid + 1
+
+def binary_search(mat,target):         
+
+    N, M = len(mat) ,len(mat[0])
+    low  = 0 
+    high = N - 1
+
+
+    while low <= high:
+        mid = (low+high)//2
+        if mat[mid][0] <= target <= mat[mid][M-1]:
+            return binary_search_on_row(mat,target,mid)
+            
+        elif mat[mid][M-1] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
