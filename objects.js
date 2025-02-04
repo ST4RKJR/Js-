b2e3d431d9044f1b1d1211aaae732f232d487bf5
Stark <sahil.gen9@gmail.com>
2025-02-04 09:20:20 +0530

    objects.js

---
+++ objects.js
@@ -0,0 +1,12 @@
+let car = {
+    brand :"Koegnissegg",
+    model :"Jesko",
+    color :"Red",
+    speed(){
+        "Runs on the Speed of 500Km/hr"
+    },
+}
+
+console.log(Object.keys(car))
+
+console.log(car.forEach(Object.typeof ))
