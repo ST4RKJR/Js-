b49efad6dc2e261dd682f4c63b95bc1ef99d7801
Stark <sahil.gen9@gmail.com>
2025-02-04 09:52:34 +0530

    objects.js

--- objects.js
+++ objects.js
@@ -9,4 +9,58 @@ let car = {
 
 console.log(Object.keys(car))
 
-console.log(car.forEach(Object.typeof ))
+
+// let car = {
+//     brand: "Koenigsegg",
+//     model: "Jesko",
+//     color: "Red",
+//     speed() {
+//         return "Runs on the Speed of 500Km/hr";
+//     },
+// };
+
+// for (let [key, value] of Object.entries(car)) {
+//     console.log(`${key}: ${typeof value}`);
+// }
+
+let methods = Object.keys(car).filter(k =>{
+    return typeof car[k] === "function"
+})
+
+console.methods
+
+
+//how to use object.entries and object.fromentries
+
+
+//ShallowCopy and DeepCopy
+const vamshi = {
+    age : 19,
+    lc : 850,
+    githubStreak: {
+        count : 21,
+        unit : "days"
+    },
+    githubHandle: "dingdong_vamshi",
+    voulnteer(){
+        return "Feeds street dogs"
+    },
+    play(){
+        console.log("Throws a volleyball at your face!")
+    },
+    dance(){
+        return "breaks the floor"
+    }
+}
+
+
+vamshi.lc = 1050
+
+const imposter = vamshi 
+
+imposter.lc = 1050
+
+
+
+
+
