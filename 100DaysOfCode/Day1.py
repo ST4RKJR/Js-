6eaa7ffa06742e91f1ee5fd99a4fb4e69d5ba28a
Stark <sahil.gen9@gmail.com>
2025-02-14 12:23:25 +0530

    Day1.py

---
+++ 100DaysOfCode/Day1.py
@@ -0,0 +1,16 @@
+#Kth largest element in an Array
+
+
+#It will work for large time limit
+def findKthLargest(nums, k):
+    copy = nums
+    new = []
+    for i in range(k-1):
+        new.append(max(copy))
+        copy.remove(max(copy))
+    return max(copy)
+
+#It will work in shorter time period
+def findKthLargest(nums, k):
+    nums.sort()
+    return nums[-k]
