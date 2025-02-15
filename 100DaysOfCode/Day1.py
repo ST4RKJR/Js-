920e3b117568067287b36890e59f41d2ceca1969
Stark <sahil.gen9@gmail.com>
2025-02-15 09:44:29 +0530

    Day1.py

--- 100DaysOfCode/Day1.py
+++ 100DaysOfCode/Day1.py
@@ -2,13 +2,13 @@
 
 
 #It will work for large time limit
-def findKthLargest(nums, k):
-    copy = nums
-    new = []
-    for i in range(k-1):
-        new.append(max(copy))
-        copy.remove(max(copy))
-    return max(copy)
+# def findKthLargest(nums, k):
+#     copy = nums
+#     new = []
+#     for i in range(k-1):
+#         new.append(max(copy))
+#         copy.remove(max(copy))
+#     return max(copy)
 
 #It will work in shorter time period
 def findKthLargest(nums, k):
