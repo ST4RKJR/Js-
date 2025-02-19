71154d4770dac8ac6b7b2cd6835b33afd8efe7b7
Stark <sahil.gen9@gmail.com>
2025-02-19 19:27:28 +0530

    Day2.py

--- 100DaysOfCode/Day2.py
+++ 100DaysOfCode/Day2.py
@@ -11,3 +11,16 @@ def maxScore(s):
             onecount-=1
         maxi = max(maxi,onecount+zerocunt)
     return(maxi)
+
+
+#100 Days of Code Completed?
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+valid=True
+for i in range(100):
+    if arr1[i]==1 or arr2[i]==1:
+        pass
+    else:
+        valid=False
+        break
+print(valid)
