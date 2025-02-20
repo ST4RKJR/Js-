aa24a8124521a6ebbfe62879f3f71261e14598eb
Stark <sahil.gen9@gmail.com>
2025-02-21 00:34:34 +0530

    BubbleSortDec

---
+++ Assignment/BubbleSortDec.py
@@ -0,0 +1,9 @@
+def bubble_sort_descending(array):
+    for i in range(len(array)):
+        for j in range(len(array)-i-1):
+            if array[j] < array[j+1]:
+                array[j],array[j+1] = array[j+1],array[j]
+    return array
+
+array = [1,2,3,4,5]
+print(bubble_sort_descending(array))
