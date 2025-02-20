78310f75c53eb23751610cdbcd9c6e38562e2b62
Stark <sahil.gen9@gmail.com>
2025-02-20 10:38:52 +0530

    Re vision

---
+++ Revision.js
@@ -0,0 +1,25 @@
+// array = [1,2,3,4,5,6,7,8,9,10]
+// let EvenFilteredArray = array.filter((num)=>{if(num%2==0){
+//     console.log(`Even Number: ${num}`)
+// }})
+
+// let OddFilteredArray = array.filter((num)=>{if(num%2!==0){
+//     console.log(`Odd Number: ${num}`)
+// }})
+
+// //CRUD OPERATIONS
+// let object = {
+//     key : "value"
+// }
+
+
+// // object.name1="sahil"
+// // object.name2="hari"
+// // console.log(object)
+// // console.log(object.name2)
+// // object.name2="harikrushna"
+// // console.log(object)
+// // object.delete(name2)
+// // console.log(object)
+
+
