3e52b9251658cc499b7a19df53893695a664900b
Stark <sahil.gen9@gmail.com>
2025-02-22 22:52:14 +0530

    Day4.py

---
+++ 100DaysOfCode/Day4.py
@@ -0,0 +1,20 @@
+#Minimum Size Subarray Sum K
+def minSubArray(arr, k):
+    l = len(arr)
+    mini = float('inf')
+    x = 0
+    summ = 0
+
+    for y in range(l):
+        summ += arr[y]
+
+        while summ >= k:
+            mini = min(mini, y - x + 1)
+            summ -= arr[x]
+            x += 1
+
+    if mini != float("inf"):
+        return mini 
+    else:
+        return 0
+    
