f60623694d81a0bd36e5d7c360219140c701cddf
Stark <sahil.gen9@gmail.com>
2025-02-23 22:43:15 +0530

    Day4.py

--- 100DaysOfCode/Day4.py
+++ 100DaysOfCode/Day4.py
@@ -19,3 +19,37 @@ def minSubArray(arr, k):
         return 0
     
 #Prefix Removals
+# Your code here
+def longest_prefix_in_substring(s):
+    n = len(s)
+    for i in range(n):
+        prefix = s[:i + 1]
+        if s.find(prefix, i + 1) != -1:
+            return i + 1 
+    return 0 
+
+def process_string(s):
+    while True:
+        x = longest_prefix_in_substring(s)
+        if x == 0:
+            break
+        s = s[x:]
+    return s
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+    
+    t = int(data[0]) 
+    results = []
+    
+    for i in range(1, t + 1):
+        s = data[i]
+        final_string = process_string(s)
+        results.append(final_string)
+    
+    print("\n".join(results))
+
+if __name__ == "__main__":
+    main()
