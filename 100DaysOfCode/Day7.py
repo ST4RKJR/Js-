ceebbea26bf661596c3f58c7d27115e9c4442276
Stark <sahil.gen9@gmail.com>
2025-02-24 11:02:21 +0530

    Day7.py

--- 100DaysOfCode/Day7.py
+++ 100DaysOfCode/Day7.py
@@ -16,3 +16,15 @@ for num in A:
         map[remainder] = 1
 print(count)
 
+#Little Alice and Numbers
+N = int(input())
+arr = list(map(int, input().split()))
+Q = int(input())
+prefix = [0] * (N + 1)
+
+for i in range(N):
+    prefix[i + 1] = prefix[i] + (arr[i] % 5 == 0)
+
+for _ in range(Q):
+    l, r = map(int, input().split())
+    print(prefix[r + 1] - prefix[l])
