330519bbf93b0e5452a509ed488e5754900950ca
Stark <sahil.gen9@gmail.com>
2025-02-25 16:18:11 +0530

    raghav.js

--- raghav.js
+++ raghav.js
@@ -1,19 +1,26 @@
-// myPromise = new Promise((resolve,reject)=>{
-//     console.log("entered Promise");
-//     let status = false;
-//     if (status){
+// // myPromise = new Promise((resolve,reject)=>{
+// //     console.log("entered Promise");
+// //     let status = false;
+// //     if (status){
 
-//     }
-// })
+// //     }
+// // })
 
-function authenticateUser(username, password, users) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (users[username] === password) {
-                resolve(`Login successful for user: ${username}`);
-            } else {
-                reject("Authentication failed. Invalid username or password.");
-            }
-        }, 3000);
-    });
-}
+// function authenticateUser(username, password, users) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (users[username] === password) {
+//                 resolve(`Login successful for user: ${username}`);
+//             } else {
+//                 reject("Authentication failed. Invalid username or password.");
+//             }
+//         }, 3000);
+//     });
+// }
+
+
+
+
+let obj = {name:"sahil",age:19}
+let k = obj.keys()
+console.log(k)
