e010297de823145e64abfed458e4ec5e145cb1b7
Stark <sahil.gen9@gmail.com>
2025-02-25 16:17:41 +0530

    SelectionSort.py

---
+++ Sorts/SelectionSort.py
@@ -0,0 +1,14 @@
+def selection_sort(seq):
+    # mini = seq[0]
+    # for i in range(len(seq)):
+    #     for j in range(i,len(seq)):
+    #     if seq[i] < mini:
+    #         mini = seq[i]
+    # return mini
+    for i in range(len(seq)):
+        min_index = i
+        for j in range(i+1,len(seq)):
+            if(seq[j] < seq[min_index]):
+                min_index = j
+        seq[i],seq[min_index] = seq[min_index],seq[i]
+    return seq
