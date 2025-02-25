163608a99021ac080b634439c7f4da5f446f6c44
Stark <sahil.gen9@gmail.com>
2025-02-25 16:17:32 +0530

    InsertionSort.py

---
+++ Sorts/InsertionSort.py
@@ -0,0 +1,12 @@
+# array = list(map(int,input().split()))
+# sorte = []
+# for i in range(len(array)):
+#     sorte.append(array[i])
+#     j = len(sorte)-1
+#     while (j>0 and array[j-1]<array[j]):
+#         array[j],array[j-1] = array[j-1],array[j]
+#         j -= 1
+# print(array[::-1])
+    
+print(448/84)
+print(3599/365)
