473f2da7de7e6ef2c9d5dc7bd86303a5495157ef
Stark <sahil.gen9@gmail.com>
2025-03-03 19:28:21 +0530

    Day18.py

---
+++ 100DaysOfCode/Day18.py
@@ -0,0 +1,12 @@
+#Implementing insertion sort
+def insertion_sort(seq):
+    for i in range(1, len(seq)):
+        key = seq[i]
+        j = i - 1
+        while j >= 0 and seq[j] > key:
+            seq[j + 1] = seq[j]
+            j -= 1
+        seq[j + 1] = key
+    return seq
+
+
