5e1502b2ae25a2c4642f4f5b8793a9e404cac8b3
Stark <sahil.gen9@gmail.com>
2025-03-04 23:17:45 +0530

    Day19.py

--- 100DaysOfCode/Day19.py
+++ 100DaysOfCode/Day19.py
@@ -13,3 +13,25 @@ def minEatingSpeed(piles, h):
     
     return left
 
+#Square Root Of an Integer
+# Your code here
+def floor_sqrt(n):
+    if n == 0 or n == 1:
+        return n
+    left, right = 1, n
+    ans = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid == n:
+            return mid
+        elif mid * mid < n:
+            ans = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return ans
+
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    print(floor_sqrt(n))
