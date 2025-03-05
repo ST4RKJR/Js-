e788b3ae74845f401c18f1d13195b4812c35e6f2
Stark <sahil.gen9@gmail.com>
2025-03-05 19:39:22 +0530

    Day12.py

--- 100DaysOfCode/Day12.py
+++ 100DaysOfCode/Day12.py
@@ -6,3 +6,23 @@ def numOfFreq(arr, queries):
     return result
 
 
+#First and Last position of Key in array 
+def searchRange(nums, target):
+    def find_bound(is_first):
+        left, right = 0, len(nums) - 1
+        bound = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                bound = mid
+                if is_first:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return bound
+    
+    return [find_bound(True), find_bound(False)]
