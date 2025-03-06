68623b2f15f1a51e46f66e697e3cf7ef04364694
Stark <sahil.gen9@gmail.com>
2025-03-06 23:34:53 +0530

    Day21.py

--- 100DaysOfCode/Day21.py
+++ 100DaysOfCode/Day21.py
@@ -30,3 +30,33 @@ class Solution:
                 right -= 1
         
         return max_area
+    
+#Subarrays Sum Equals Zero
+def subarray_exists(arr, n):
+#Write your code here
+
+
+    # def subArrayExists(self,arr):
+    prefix = [0] * (len(arr) + 1)
+    for i in range(1, len(arr) + 1):
+        prefix[i] = prefix[i-1] + arr[i-1]
+    st = set(prefix)
+    if (len(st) == len(prefix)):
+        return False
+    else:
+        return True
+    
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr):
+        prefix = [0] * (len(arr) + 1)
+        for i in range(1, len(arr) + 1):
+           prefix[i] = prefix[i-1] + arr[i-1]
+        st = set(prefix)
+        if (len(st) == len(prefix)):
+            return False
+        else:
+            return True
