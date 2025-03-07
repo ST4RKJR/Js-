4048d87cc33a1ca5df6ea783fbca588717cafb37
Stark <sahil.gen9@gmail.com>
2025-03-07 19:09:29 +0530

    Day22.py

--- 100DaysOfCode/Day22.py
+++ 100DaysOfCode/Day22.py
@@ -30,6 +30,22 @@ def has_pair_with_sum(arr, target):
             right -= 1
     return False
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            max_area = max(max_area, (right - left) * min(height[left], height[right]))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
+    
 #User function Template for python3
 class Solution:
     def twoSum(self, arr, target):
