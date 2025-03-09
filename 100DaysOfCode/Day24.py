c5a255c7db6dd493197f56b44b99f04d38f294e9
Stark <sahil.gen9@gmail.com>
2025-03-09 22:10:44 +0530

    Day24.py

--- 100DaysOfCode/Day24.py
+++ 100DaysOfCode/Day24.py
@@ -4,3 +4,28 @@ for i in range(2**N):
     print(bin(i)[2:].zfill(N))
 
 
+class Solution:
+    def validStrings(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        self.generateStrings(result, [], n, -1)
+        return result
+
+    def generateStrings(self, result, sb, n, lastChar):
+        if len(sb) == n:
+            result.append(''.join(sb))
+            return
+
+        # Add '1' to the string
+        sb.append('1')
+        self.generateStrings(result, sb, n, 1)
+        sb.pop()
+
+        # Add '0' to the string, only if the last character was not '0'
+        if lastChar != 0:
+            sb.append('0')
+            self.generateStrings(result, sb, n, 0)
+            sb.pop()
