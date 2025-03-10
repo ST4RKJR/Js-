a8d9fcd61ebf18b7f75ee765596244c0b0df6d3d
Stark <sahil.gen9@gmail.com>
2025-03-10 23:14:08 +0530

    MatrixMul.py

--- 100DaysOfCode/Day25.py
+++ 100DaysOfCode/Day25.py
@@ -9,8 +9,8 @@ def maxAbsoluteSum(nums):
     return max(total_max, abs(total_min))
 
 class Solution:
-    def maxAbsoluteSum(self, nums: List[int]) -> int:
-        return max(0, max(K:=list(accumulate(nums))))-min(0, min(K))
+    def maxAbsoluteSum(self, nums) -> int:
+        return max(0, max(K:=list((nums))))-min(0, min(K))
         
         
 
