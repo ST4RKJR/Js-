6c9aa1856b25986c69d036e067bf5d669cec18b2
Stark <sahil.gen9@gmail.com>
2025-03-10 23:11:23 +0530

    Day25.py

--- 100DaysOfCode/Day25.py
+++ 100DaysOfCode/Day25.py
@@ -7,3 +7,14 @@ def maxAbsoluteSum(nums):
         total_max = max(total_max, max_sum)
         total_min = min(total_min, min_sum)
     return max(total_max, abs(total_min))
+
+#Count Even Factors
+n = int(input())
+count = 0
+for i in range(1, int(n**0.5) + 1):
+    if n % i == 0:
+        if i % 2 == 0:
+            count += 1
+        if (n // i) % 2 == 0 and i != n // i:
+            count += 1
+print(count)
