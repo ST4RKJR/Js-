e874d801a6a99250c99c7aa582827bce55b15253
Stark <sahil.gen9@gmail.com>
2025-03-12 22:02:30 +0530

    Day26.py

--- 100DaysOfCode/Day26.py
+++ 100DaysOfCode/Day26.py
@@ -5,3 +5,12 @@ def tri(n):
     elif n == 1 or n == 2:
         return 1
     
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 1
+    
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    
+    return dp[n]
+
+
