ba7e82dd0b62f3b030c2b242bd19bf1b542e5aa9
Stark <sahil.gen9@gmail.com>
2025-03-12 22:02:57 +0530

    Day26.py

--- 100DaysOfCode/Day26.py
+++ 100DaysOfCode/Day26.py
@@ -14,3 +14,21 @@ def tri(n):
     return dp[n]
 
 
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+        
+        x = 0
+        y = 1
+        z = 1
+        
+        for i in range(3, n + 1):
+            x, y, z = y, z, x + y + z
+            
+        return z        
+
+
+
