9ee2ad786e49b13d445b7c6771ea799c4fcbfaac
Stark <sahil.gen9@gmail.com>
2025-03-13 18:27:41 +0530

    Day27.py

--- 100DaysOfCode/Day27.py
+++ 100DaysOfCode/Day27.py
@@ -6,3 +6,12 @@ def minCostClimbingStairs(cost):
         cost[i] += min(cost[i+1], cost[i+2])
         
     return min(cost[0], cost[1])
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        
+        for i in range(len(cost) - 4, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+            
+        return min(cost[0], cost[1])
