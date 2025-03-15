d07a61d00a4c6c9281dd71ea70f25159c2271521
Stark <sahil.gen9@gmail.com>
2025-03-15 21:36:32 +0530

    Day29.py

--- 100DaysOfCode/Day29.py
+++ 100DaysOfCode/Day29.py
@@ -4,3 +4,13 @@ def kthGrammar(n, k):
         return 0
     parent = kthGrammar(n - 1, (k + 1) // 2)
     return parent if k % 2 == 1 else 1 - parent
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        length = 2 ** (n - 2)
+        if k <= length:
+            return self.kthGrammar(n - 1, k)
+        else:
+            return 1 - self.kthGrammar(n - 1, k - length)
