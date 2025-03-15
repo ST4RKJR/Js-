d3b65c96f0c4041c3e2631a51d3892f6d80d205e
Stark <sahil.gen9@gmail.com>
2025-03-15 21:37:17 +0530

    Day29.py

--- 100DaysOfCode/Day29.py
+++ 100DaysOfCode/Day29.py
@@ -16,3 +16,13 @@ class Solution:
             return 1 - self.kthGrammar(n - 1, k - length)
         
 # Frog Jump with k Distances (Recursion)
+def frogJump(n, height, k):
+    def recur(idx):
+        if idx == 0:
+            return 0
+        min_energy = float('inf')
+        for j in range(1, min(k, idx) + 1):
+            min_energy = min(min_energy, recur(idx - j) + abs(height[idx] - height[idx - j]))
+        return min_energy
+
+    return recur(n - 1)
