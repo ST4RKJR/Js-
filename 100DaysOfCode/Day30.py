d22606eba5c1155bdd267322d5c8b5c27630bbda
Stark <sahil.gen9@gmail.com>
2025-03-16 22:36:25 +0530

    Day30.py

--- 100DaysOfCode/Day30.py
+++ 100DaysOfCode/Day30.py
@@ -36,3 +36,16 @@ def countGoodNumbers(n):
 
 
 
+class Solution:
+    # Please Upvote my solution
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        
+        def expo(x: int, num: int) -> int:
+            if num == 0:
+                return 1  
+            elif num & 1 == 0:
+                return expo(x ** 2 % MOD, num // 2)
+            return x * expo(x, num - 1) % MOD
+
+        return 5 ** (n % 2) * expo(20, n // 2) % MOD
