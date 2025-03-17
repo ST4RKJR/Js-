be671184536e62ebdf74feccccde7688858f9fe9
Stark <sahil.gen9@gmail.com>
2025-03-17 19:35:14 +0530

    Day31.py

--- 100DaysOfCode/Day31.py
+++ 100DaysOfCode/Day31.py
@@ -4,3 +4,16 @@ from itertools import chain, combinations
 def findSubsets(nums):
     return [list(subset) for subset in chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1))]
 
+#Count Subsets with Sum K- Recursion
+def countSubsets(nums, n, k):
+    if k == 0:
+        return 1
+    if n == 0:
+        return 0
+    if nums[n - 1] > k:
+        return countSubsets(nums, n - 1, k)
+    return countSubsets(nums, n - 1, k) + countSubsets(nums, n - 1, k - nums[n - 1])
+
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+print(countSubsets(nums, n, k))
