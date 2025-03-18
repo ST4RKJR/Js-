fc4e6088db02ca7eec7f1a1e86562090b6bac52e
Stark <sahil.gen9@gmail.com>
2025-03-18 17:48:02 +0530

    Day33.py

--- 100DaysOfCode/Day33.py
+++ 100DaysOfCode/Day33.py
@@ -10,3 +10,21 @@ def isPresent(arr, X):
         else:
             right = mid - 1
     return False
+
+#Find Closest Number in Sorted Array
+def findClosest(arr, K):
+    left, right = 0, len(arr) - 1
+    closest = arr[0]
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if abs(arr[mid] - K) < abs(closest - K) or (abs(arr[mid] - K) == abs(closest - K) and arr[mid] > closest):
+            closest = arr[mid]
+        
+        if arr[mid] < K:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return closest
