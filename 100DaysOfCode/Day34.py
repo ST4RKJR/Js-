80fae7738367805f2f7ef4dc3697bb8fe2257fe0
Stark <sahil.gen9@gmail.com>
2025-03-19 22:00:58 +0530

    Day34.py

--- 100DaysOfCode/Day34.py
+++ 100DaysOfCode/Day34.py
@@ -9,6 +9,20 @@ def firstBadVersion(n):
             left = mid + 1  # Search in the right half
     return left
 
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left<right:
+            mid = (left+right)//2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left 
+
 
 #Diagonal Binary Search
 def searchMatrix(matrix, target):
