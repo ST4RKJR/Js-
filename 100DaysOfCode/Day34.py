31f7d89c38d9d1b9c1dccba5efad142d6eaa1ab7
Stark <sahil.gen9@gmail.com>
2025-03-19 22:00:44 +0530

    Day34.py

--- 100DaysOfCode/Day34.py
+++ 100DaysOfCode/Day34.py
@@ -9,3 +9,25 @@ def firstBadVersion(n):
             left = mid + 1  # Search in the right half
     return left
 
+
+#Diagonal Binary Search
+def searchMatrix(matrix, target):
+    n = len(matrix)
+    row, col = 0, n - 1
+
+    while row < n and col >= 0:
+        if matrix[row][col] == target:
+            print(True)
+            return
+        elif matrix[row][col] > target:
+            col -= 1
+        else:
+            row += 1
+
+    print(False)
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+target = int(input())
+
+searchMatrix(matrix, target)
