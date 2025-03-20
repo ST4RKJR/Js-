a80f7d3cedeb1f4e126f093eaba1d19aca624001
Stark <sahil.gen9@gmail.com>
2025-03-20 22:26:42 +0530

    Day35.py

--- 100DaysOfCode/Day35.py
+++ 100DaysOfCode/Day35.py
@@ -10,3 +10,14 @@ import math
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         return int(-1+math.sqrt(1+8*n))//2
+    
+#Find Peak Element
+def findPeakElement(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
