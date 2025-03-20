8f6c9b9558e3513b0c523430be324ad08c739b61
Stark <sahil.gen9@gmail.com>
2025-03-20 22:25:38 +0530

    Day35.py

--- 100DaysOfCode/Day35.py
+++ 100DaysOfCode/Day35.py
@@ -5,3 +5,7 @@ def arrangeCoins(x):
         rows_covered += 1
         x -= 1
     return rows_covered
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        return int(-1+sqrt(1+8*n))//2
