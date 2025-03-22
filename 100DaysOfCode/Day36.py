aff97a9b3fdcd2ed1e8f1c57111f869fab40aa54
Stark <sahil.gen9@gmail.com>
2025-03-22 19:44:47 +0530

    Day36.py

--- 100DaysOfCode/Day36.py
+++ 100DaysOfCode/Day36.py
@@ -54,3 +54,20 @@ def find_special_integer(arr):
         
         if right - left > threshold:
             return num
+        
+class Solution:
+    def findSpecialInteger(self, arr) -> int:
+        if len(arr) < 2:
+            return arr[0]
+        result = 0
+        percent = int(len(arr) * 0.25)
+        count_map = {}
+
+        for i in arr:
+            count_map[i] = count_map.get(i, 0) + 1
+
+        for key, count in count_map.items():
+            if count > percent:
+                result = key
+
+        return result
