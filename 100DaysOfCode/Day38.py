96b92e711555e0f6449b55c252fb1f64902129da
Stark <sahil.gen9@gmail.com>
2025-03-23 20:13:23 +0530

    Day38.py

--- 100DaysOfCode/Day38.py
+++ 100DaysOfCode/Day38.py
@@ -8,3 +8,13 @@ def maxProfit(prices):
         max_profit = max(max_profit, price - min_price)
     
     return max_profit
+
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        for cost in range(1, len(prices)):
+            max_profit = max(max_profit, prices[cost]-min_price)
+            min_price = min(min_price, prices[cost])
+        return max_profit
