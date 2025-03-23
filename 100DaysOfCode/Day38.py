6d1b7761e1145d1bcef9f8d7f84dae76e95a9378
Stark <sahil.gen9@gmail.com>
2025-03-23 20:13:50 +0530

    Day38.py

--- 100DaysOfCode/Day38.py
+++ 100DaysOfCode/Day38.py
@@ -18,3 +18,13 @@ class Solution:
             max_profit = max(max_profit, prices[cost]-min_price)
             min_price = min(min_price, prices[cost])
         return max_profit
+    
+    
+class Solution:
+    def minCostClimbingStairs(self, cost) -> int:
+        cost.append(0)
+        
+        for i in range(len(cost) - 4, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+            
+        return min(cost[0], cost[1])
