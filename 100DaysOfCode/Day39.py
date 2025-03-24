da435d0237bc7b3dbf2a564d46701685010a221d
Stark <sahil.gen9@gmail.com>
2025-03-24 19:35:58 +0530

    Day39.py

--- 100DaysOfCode/Day39.py
+++ 100DaysOfCode/Day39.py
@@ -46,6 +46,27 @@ class Solution:
         
         
 #Frog Jump (DP)
+d={}
+def frogJump(heights,i=-1):
+    # Write your code here
+    if i in d:
+        return d[i]
+    if i == -1:
+        d[0]=0
+        if len(heights)>1:
+            d[1] = abs(heights[0]-heights[1])
+
+        i = len(heights)-1
+    if i == 0 :
+        return 0
+    if i == 1:
+        return abs(heights[0]-heights[1])
+    a = frogJump(heights,i-1)+abs(heights[i]-heights[i-1])
+    b= frogJump(heights,i-2)+abs(heights[i]-heights[i-2])
+    d[i] = min(a,b)
+    return min(a,b)
+
+
 class Solution:
     def maxProfit(self, prices) -> int:
         min_price = float('inf')
