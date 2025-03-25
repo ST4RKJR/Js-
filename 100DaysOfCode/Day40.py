5f563aa8770cc55e1f7d9b50c0f1ade997c5de41
Stark <sahil.gen9@gmail.com>
2025-03-25 19:33:02 +0530

    Day40.py

--- 100DaysOfCode/Day40.py
+++ 100DaysOfCode/Day40.py
@@ -8,3 +8,19 @@ def climbTree(n):
 
 n = int(input())
 print(climbTree(n))
+
+
+#Perfect squares Combinations
+import math
+
+def numSquares(N):
+    dp = [float('inf')] * (N + 1)
+    dp[0] = 0
+
+    for i in range(1, N + 1):
+        j = 1
+        while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+            j += 1
+    
+    return dp[N]
