3b3da8342c178f31cc067dae64b2a64e8f044373
Stark <sahil.gen9@gmail.com>
2025-03-25 19:35:34 +0530

    Day40.py

--- 100DaysOfCode/Day40.py
+++ 100DaysOfCode/Day40.py
@@ -10,6 +10,19 @@ n = int(input())
 print(climbTree(n))
 
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        return self.helper(n, memo)
+    
+    def helper(self, n: int, memo: dict[int, int]) -> int:
+        if n == 0 or n == 1:
+            return 1
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
+
+
 #Perfect squares Combinations
 import math
 
