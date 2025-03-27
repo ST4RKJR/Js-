69f419bf15d211af78391f495d6b73dc6bc13385
Stark <sahil.gen9@gmail.com>
2025-03-27 19:14:50 +0530

    Day42.py

--- 100DaysOfCode/Day42.py
+++ 100DaysOfCode/Day42.py
@@ -25,3 +25,19 @@ class Solution:
             q[v%3] = sorted(q[v%3]+[v])[:2]
 
         return w - min(q[w%3][0], sum(q[w%3%2+1]))
+    
+#Max Sum Non adjacent Numbers
+def maxSum(n, nums):
+    if n == 1:
+        return nums[0]
+    
+    dp = [0] * n
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    
+    for i in range(2, n):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+    
+    return dp[-1]
+
+
