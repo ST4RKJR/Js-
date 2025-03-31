c28d9b2828c6c3638f7ba28229db929ff8dfa061
Stark <sahil.gen9@gmail.com>
2025-03-31 15:20:15 +0530

    Day4 6.py

--- 100DaysOfCode/Day46.py
+++ 100DaysOfCode/Day46.py
@@ -16,3 +16,37 @@ n = int(input())
 prime_xor(n)
 
 #Prime Factors of X
+def sieve_factorization(limit):
+    spf = list(range(limit + 1))  
+    for i in range(2, int(limit ** 0.5) + 1):
+        if spf[i] == i:  
+            for j in range(i * i, limit + 1, i):
+                if spf[j] == j:
+                    spf[j] = i  
+    return spf
+
+def get_prime_factors(x, spf):
+    factors = []
+    while x > 1:
+        factors.append(spf[x])
+        x //= spf[x]
+    return factors
+
+def solve():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    N = int(data[0])
+    arr = list(map(int, data[1:N+1]))
+
+    limit = 10**6
+    spf = sieve_factorization(limit)
+
+    results = []
+    for num in arr:
+        results.append(" ".join(map(str, get_prime_factors(num, spf))))
+    
+    sys.stdout.write("\n".join(results) + "\n")
+
+solve()
