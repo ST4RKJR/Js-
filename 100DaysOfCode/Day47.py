b5fd35443438c8fc8f87135449dcd65c319bfdf4
Stark <sahil.gen9@gmail.com>
2025-04-01 22:04:18 +0530

    Day47.py

--- 100DaysOfCode/Day47.py
+++ 100DaysOfCode/Day47.py
@@ -8,4 +8,18 @@ def climb(x):
 x = int(input())
 print(climb(x))
 
-
+#Find TargetSumWays 
+def findTargetSumWays(nums, target):
+    from collections import defaultdict
+    
+    dp = defaultdict(int)
+    dp[0] = 1  # Base case: one way to make sum 0 (using no elements)
+    
+    for num in nums:
+        next_dp = defaultdict(int)
+        for summation in dp:
+            next_dp[summation + num] += dp[summation]
+            next_dp[summation - num] += dp[summation]
+        dp = next_dp  # Move to the next iteration
+    
+    return dp[target]
