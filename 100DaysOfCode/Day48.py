f599791596d815aff57b3c0a928e9eb2f9773de3
Stark <sahil.gen9@gmail.com>
2025-04-02 22:24:34 +0530

    Day48.py

--- 100DaysOfCode/Day48.py
+++ 100DaysOfCode/Day48.py
@@ -34,3 +34,17 @@ def merge(intervals):
             merged[-1][1] = max(merged[-1][1], interval[1])
     
     return merged
+
+class Solution:
+    def merge(self, intervals):
+        intervals = sorted(intervals, key=lambda x: x [0])
+
+        ans = []
+
+        for interval in intervals:
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+        
+        return ans
