dcb4c9a63eeacfc46553eea57f466c16b9622088
Stark <sahil.gen9@gmail.com>
2025-04-05 20:04:28 +0530

    Day51.py

--- 100DaysOfCode/Day51.py
+++ 100DaysOfCode/Day51.py
@@ -18,3 +18,26 @@ def findMid(head) :
         fast = fast.next.next
 
     return slow.data
+
+
+#Insert node at at given position
+class Node:
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+
+def addElement(head, M, K):
+    new_node = Node(K)
+    
+    if M == 1:
+        new_node.next = head
+        return new_node
+
+    current = head
+    for _ in range(M - 2):
+        current = current.next
+
+    new_node.next = current.next
+    current.next = new_node
+    
+    return head
