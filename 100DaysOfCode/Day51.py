277af0e9289e94701eda25edc8dee7ee6169fc93
Stark <sahil.gen9@gmail.com>
2025-04-05 21:15:51 +0530

    Day51.py

--- 100DaysOfCode/Day51.py
+++ 100DaysOfCode/Day51.py
@@ -22,22 +22,22 @@ def findMid(head) :
 
 #Insert node at at given position
 class Node:
-    def __init__(self, data): 
-        self.data = data
-        self.next = None
-
+     def __init__(self, data): 
+         self.data = data
+         self.next = None
+ 
 def addElement(head, M, K):
-    new_node = Node(K)
-    
-    if M == 1:
-        new_node.next = head
-        return new_node
-
-    current = head
-    for _ in range(M - 2):
-        current = current.next
-
-    new_node.next = current.next
-    current.next = new_node
-    
-    return head
+     new_node = Node(K)
+     
+     if M == 1:
+         new_node.next = head
+         return new_node
+ 
+     current = head
+     for _ in range(M - 2):
+         current = current.next
+ 
+     new_node.next = current.next
+     current.next = new_node
+     
+     return head
