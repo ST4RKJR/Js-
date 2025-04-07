60de56edc78e62a1db02efee5dc855fb9d79ef23
Stark <sahil.gen9@gmail.com>
2025-04-07 18:08:11 +0530

    Day53.py

--- 100DaysOfCode/Day53.py
+++ 100DaysOfCode/Day53.py
@@ -42,3 +42,22 @@ def Merge(head1, head2):
         current.next = head2
     
     return dummy.next
+
+
+    
+    
+class Solution:
+    def mergeTwoLists(self, list1):
+        cur = dummy = 2
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
