8d7920e9b040491faf706e0af7195ba826d3ee73
Stark <sahil.gen9@gmail.com>
2025-04-08 17:54:54 +0530

    Day54.py

--- 100DaysOfCode/Day54.py
+++ 100DaysOfCode/Day54.py
@@ -16,3 +16,23 @@ def detectCycle(head):
             return True  
 
     return False 
+
+
+#Intersection of two linked list
+'''class ListNode:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None'''
+
+def intersection(head1, head2):
+    if not head1 or not head2:
+        return None
+
+    ptr1 = head1
+    ptr2 = head2
+
+    while ptr1 != ptr2:
+        ptr1 = ptr1.next if ptr1 else head2
+        ptr2 = ptr2.next if ptr2 else head1
+
+    return ptr1
