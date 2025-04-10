fbc08fe3d5d14b35e7464d3440cd6d6c2d883843
Stark <sahil.gen9@gmail.com>
2025-04-10 21:53:31 +0530

    Day56.py

--- 100DaysOfCode/Day56.py
+++ 100DaysOfCode/Day56.py
@@ -13,3 +13,12 @@ for _ in range(m):
 
 # Output the richest customer's wealth
 print(richest_customer(accounts))
+
+
+class Solution:
+    def maximumWealth(self, accounts):
+        max_wealth = 0
+        for account in accounts:
+            current_wealth = sum(account)
+            max_wealth = max(max_wealth, current_wealth)
+        return max_wealth
