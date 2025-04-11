aff6bd58277e142843d84defe9714cc78f8ad659
Stark <sahil.gen9@gmail.com>
2025-04-11 18:09:37 +0530

    Day57.py

--- 100DaysOfCode/Day57.py
+++ 100DaysOfCode/Day57.py
@@ -16,3 +16,14 @@ def removeOuterParentheses(S):
 
 S = input()
 print(removeOuterParentheses(S))
+
+
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        res, opened = [], 0
+        for c in S:
+            if c == '(' and opened > 0: res.append(c)
+            if c == ')' and opened > 1: res.append(c)
+            opened += 1 if c == '(' else -1
+        
+        return "".join(res)
