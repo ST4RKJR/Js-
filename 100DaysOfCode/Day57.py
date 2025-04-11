54092512af2b80bc158a4eecf92219f41e94f9f6
Stark <sahil.gen9@gmail.com>
2025-04-11 18:11:57 +0530

    Day57.py

--- 100DaysOfCode/Day57.py
+++ 100DaysOfCode/Day57.py
@@ -27,3 +27,36 @@ class Solution:
             opened += 1 if c == '(' else -1
         
         return "".join(res)
+    
+    
+    
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+def pairSum(head, K):
+    # Initialize pointers to the beginning and end of the list
+    left = head
+    right = head
+
+    # Move the right pointer to the last node
+    while right.next:
+        right = right.next
+
+    count = 0
+
+    # Traverse the list with two pointers
+    while left != right and left.prev != right:
+        current_sum = left.data + right.data
+        if current_sum == K:
+            count += 1
+            left = left.next
+            right = right.prev
+        elif current_sum < K:
+            left = left.next
+        else:
+            right = right.prev
+
+    return count
