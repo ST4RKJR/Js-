88c8057636ab2e4ce5cb284e086a1250fe1a22a7
Stark <sahil.gen9@gmail.com>
2025-04-14 22:12:25 +0530

    Day60.py

--- 100DaysOfCode/Day60.py
+++ 100DaysOfCode/Day60.py
@@ -39,3 +39,26 @@ def max_list(num1, num2):
     else:
         # Same length, compare lexicographically
         return max(int(str1), int(str2))
+    
+    
+    
+class Solution:
+    def minSubArrayLen(self, k: int, arr) -> int:
+        l = len(arr)
+        mini = float('inf')
+        x = 0
+        summ = 0
+
+        for y in range(l):
+            summ += arr[y]
+
+            while summ >= k:
+                mini = min(mini, y - x + 1)
+                summ -= arr[x]
+                x += 1
+
+        if mini != float("inf"):
+            return mini 
+        else:
+            return 0
+        
