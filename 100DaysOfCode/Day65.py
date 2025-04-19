c61f55c2af4687535677e5ceffa7a09f0867b111
Stark <sahil.gen9@gmail.com>
2025-04-19 12:39:05 +0530

    Day65.py

--- 100DaysOfCode/Day65.py
+++ 100DaysOfCode/Day65.py
@@ -12,3 +12,24 @@ def count_nodes(head):
         temp = temp.next
         count +=1 
     return count
+
+
+def is_balanced(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return "NO"
+        else:
+            stack.append(char)
+    return "YES" if not stack else "NO"
+n = int(input())
+results = []
+for _ in range(n):
+    s = input()
+    results.append(is_balanced(s))
+for result in results:
+    print(result)
