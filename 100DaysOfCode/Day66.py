3ae4d11b76423f9ce8dabd95cb5c2b7ed213ae5f
Stark <sahil.gen9@gmail.com>
2025-04-20 21:47:28 +0530

    Day66.py

--- 100DaysOfCode/Day66.py
+++ 100DaysOfCode/Day66.py
@@ -8,4 +8,20 @@ print(count)
 
 
 
-#
+#Count Subarrays with Max Value in Range
+def numSubarrayBoundedMax(nums, left, right):
+    def count(limit):
+        c = 0 
+        curr = 0 
+        for i in nums:
+            if i  <= limit:
+                curr += 1
+                c += curr
+            else:
+                curr = 0
+
+        return c 
+
+    maxRight = count(right)
+    maxLeft = count(left - 1)
+    return maxRight - maxLeft
