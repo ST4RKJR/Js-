ddaa523e4958164275a9d75201fc5e22feb0058e
Stark <sahil.gen9@gmail.com>
2025-04-20 21:49:48 +0530

    Day66.py

--- 100DaysOfCode/Day66.py
+++ 100DaysOfCode/Day66.py
@@ -25,3 +25,25 @@ def numSubarrayBoundedMax(nums, left, right):
     maxRight = count(right)
     maxLeft = count(left - 1)
     return maxRight - maxLeft
+
+
+class Solution:
+    def numSubarrayBoundedMax(self, nums, left: int, right: int) -> int:
+        
+        def count_subarrays_with_max_atmost_x(x):
+        
+            front = count = 0
+
+            for rear in range(len(nums)):
+
+                if nums[rear] > x:
+
+                    front = rear + 1
+                    continue
+
+                count += rear - front +1
+            
+            return count
+         
+        return count_subarrays_with_max_atmost_x(right) - count_subarrays_with_max_atmost_x(left-1)
+		
