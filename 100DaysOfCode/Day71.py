111f75bb36168abda32f9a55b9e6eb566e41d4d4
Stark <sahil.gen9@gmail.com>
2025-04-25 19:29:02 +0530

    Day71.py

--- 100DaysOfCode/Day71.py
+++ 100DaysOfCode/Day71.py
@@ -9,3 +9,37 @@ for i in range(n):
     else:
         odd += arr[i]
 print(even-odd)
+
+
+#
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def build_tree(level_order):
+    if not level_order or level_order[0] == -1:
+        return None
+
+    root = TreeNode(level_order[0])
+    queue = [root]
+    i = 1
+    n = len(level_order)
+
+    while queue and i < n:
+        current = queue.pop(0)
+
+        # Left child
+        if i < n and level_order[i] != -1:
+            current.left = TreeNode(level_order[i])
+            queue.append(current.left)
+        i += 1
+
+        # Right child
+        if i < n and level_order[i] != -1:
+            current.right = TreeNode(level_order[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
