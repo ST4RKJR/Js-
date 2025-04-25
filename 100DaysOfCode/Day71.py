25452c51be39ba9b80d2cc17466889a169b08fe7
Stark <sahil.gen9@gmail.com>
2025-04-25 19:31:46 +0530

    Day71.py

--- 100DaysOfCode/Day71.py
+++ 100DaysOfCode/Day71.py
@@ -43,3 +43,21 @@ def build_tree(level_order):
         i += 1
 
     return root
+
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+
+def is_identical(root1, root2):
+    if root1 is None and root2 is None:
+        return True
+    if root1 is None or root2 is None:
+        return False
+    if root1.val != root2.val:
+        return False
+    return (is_identical(root1.left, root2.left) and
+            is_identical(root1.right, root2.right))
