e64902cce3a3f0c52523788ac936384d1d9a0039
Stark <sahil.gen9@gmail.com>
2025-04-27 17:10:55 +0530

    Day73.py

--- 100DaysOfCode/Day73.py
+++ 100DaysOfCode/Day73.py
@@ -18,3 +18,29 @@ def col_sum(mat):
         total += mat[i][col_index]
 
     return total
+
+
+
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def merge_trees(root1, root2):
+    if not root1 and not root2:
+        return None
+    if not root1:
+        return root2
+    if not root2:
+        return root1
+
+    # Merge the two nodes
+    new_node = Node(root1.val + root2.val)
+
+    # Recursively merge the left and right children
+    new_node.left = merge_trees(root1.left, root2.left)
+    new_node.right = merge_trees(root1.right, root2.right)
+
+    return new_node
