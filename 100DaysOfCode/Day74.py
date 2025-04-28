a3e18fa89914c8acd954544b4b571407d419e00b
Stark <sahil.gen9@gmail.com>
2025-04-28 19:57:14 +0530

    Day74.py

--- 100DaysOfCode/Day74.py
+++ 100DaysOfCode/Day74.py
@@ -18,3 +18,27 @@ def minBinaryTree(root):
     if root is None:
         return float('inf')  # So that empty branches are ignored
     return min(root.key, min(minBinaryTree(root.left), minBinaryTree(root.right)))
+
+
+'''
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.key = val
+        self.left = left
+        self.right = right
+'''
+class Solution:
+    def maxDepthBinaryTree(self, root):
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepthBinaryTree(root.left), self.maxDepthBinaryTree(root.right))
+
+    def minBinaryTree(self, root):
+        if root is None:
+            return float('inf')  # So that empty branches are ignored
+        return min(root.key, self.minBinaryTree(root.left), self.minBinaryTree(root.right))
+
+    def sizeBinaryTree(self, root):
+        if root is None:
+            return 0
+        return 1 + self.sizeBinaryTree(root.left) + self.sizeBinaryTree(root.right)
