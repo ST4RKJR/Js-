8e0e391466dce8817cb4715493e5978e64fc9df4
Stark <sahil.gen9@gmail.com>
2025-05-01 18:20:07 +0530

    Day77.py

--- 100DaysOfCode/Day77.py
+++ 100DaysOfCode/Day77.py
@@ -13,3 +13,19 @@ def count_occurrences(head, K):
             count += 1
         current = current.next
     return count
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def is_unival_tree(root):
+    def dfs(node, value):
+        if node is None:
+            return True
+        if node.val != value:
+            return False
+        return dfs(node.left, value) and dfs(node.right, value)
+
+    return dfs(root, root.val)
