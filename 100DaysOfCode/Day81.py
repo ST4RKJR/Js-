b07bc963b43ee31ef861c73dee9efac81c084b09
Stark <sahil.gen9@gmail.com>
2025-05-05 21:00:15 +0530

    Day81.py

--- 100DaysOfCode/Day81.py
+++ 100DaysOfCode/Day81.py
@@ -39,3 +39,20 @@ arr = list(map(int, input().split()))
 
 # Output
 print(max_subarray_sum(arr))
+
+
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        maxSum = float('-inf')
+        currentSum = 0
+        
+        for num in nums:
+            currentSum += num
+            
+            if currentSum > maxSum:
+                maxSum = currentSum
+            
+            if currentSum < 0:
+                currentSum = 0
+        
+        return maxSum
