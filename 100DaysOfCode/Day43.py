66688a8094261bfdd71cad5de638cb04e7f3d239
Stark <sahil.gen9@gmail.com>
2025-05-08 19:41:57 +0530

    Day43.py

--- 100DaysOfCode/Day43.py
+++ 100DaysOfCode/Day43.py
@@ -10,3 +10,21 @@ def sieve_of_eratosthenes(N):
 
 N = int(input())
 sieve_of_eratosthenes(N)
+
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+        
+        count = 0
+        for i in range(2, n):
+            if is_prime[i]:
+                count += 1
+                for j in range(i * i, n, i):
+                    is_prime[j] = False
+        return count
+    
