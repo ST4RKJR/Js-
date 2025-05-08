00f3479d2a53d03c56219e98b430b4f0e46a1427
Stark <sahil.gen9@gmail.com>
2025-05-08 11:38:52 +0530

    Day86

--- 100DaysOfCode/Day86.py
+++ 100DaysOfCode/Day86.py
@@ -15,3 +15,22 @@ def node_position(head, X):
         curr = curr.next
         position += 1
     return position
+
+'''
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.left = None
+        self.right = None
+'''
+
+
+def search_in_bst(root, key):
+    if not root:
+        return 0
+    if root.data == key:
+        return 1
+    elif key < root.data:
+        return search_in_bst(root.left, key)
+    else:
+        return search_in_bst(root.right, key)
