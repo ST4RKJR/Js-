084381b10758f5e32328ff76ab98acff6c6fc149
Stark <sahil.gen9@gmail.com>
2025-05-09 11:43:07 +0530

    DailyStreak

--- 100DaysOfCode/Day88.py
+++ 100DaysOfCode/Day88.py
@@ -18,3 +18,35 @@ def count_ways(arr):
         return dp[i]
 
     return dfs(0)
+
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def level_order_traversal(root):
+    if not root:
+        return []
+
+    result = []
+    q = deque([root])
+
+    while q:
+        level_size = len(q)
+        level_nodes = []
+
+        for _ in range(level_size):
+            node = q.popleft()
+            level_nodes.append(node.val)
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        result.append(level_nodes)
+
+    return result
