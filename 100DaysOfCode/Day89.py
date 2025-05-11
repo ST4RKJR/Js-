b7a02eb3f1a98f5e24cca4124f00486da9d5e839
Stark <sahil.gen9@gmail.com>
2025-05-11 18:30:49 +0530

    100DaysOfCode

--- 100DaysOfCode/Day89.py
+++ 100DaysOfCode/Day89.py
@@ -20,3 +20,20 @@ def maxWeight(wt, val, W):
     memo = [[-1] * (W + 1) for _ in range(n + 1)]
 
     return f(W, val, wt, n, memo)
+
+
+# Merge Sorted Array 
+def merge(nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1 
+    right = m + n - 1
+
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[right] = nums1[i]
+            i -= 1
+        else:
+            nums1[right] = nums2[j]
+            j -= 1
+
+        right -= 1
