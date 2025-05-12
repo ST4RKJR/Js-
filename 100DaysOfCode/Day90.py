b28367388da61baf53a3da04fa54252548fbb50f
Stark <sahil.gen9@gmail.com>
2025-05-12 18:32:17 +0530

    100DaysOfCode

--- 100DaysOfCode/Day90.py
+++ 100DaysOfCode/Day90.py
@@ -26,3 +26,18 @@ def min_diff(root):
     dfs(root)
 
     return 
+
+
+## ALTERNATE ELEMENTS
+n = int(input())
+arr = [int(i) for i in input().split()]
+
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         print(arr[i], end = " ")
+
+# for i in range(0, n, 2):
+#     print(arr[i], end = " ")
+
+print(*arr[::2])
