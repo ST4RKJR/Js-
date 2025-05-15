3c453601818c25050c3e61308508fcff80dd7b0d
Stark <sahil.gen9@gmail.com>
2025-05-15 18:35:36 +0530

    100DaysOfCode

--- 100DaysOfCode/Day93.py
+++ 100DaysOfCode/Day93.py
@@ -7,3 +7,16 @@ for i in range(n):
 
 for i in range(n):
     print(*mat[i])
+    
+## MINIMUM DIFFRENCE
+n = int(input())
+arr = [int(i) for i in input().split()]
+
+arr.sort()
+target = arr[n // 2]
+
+ans = 0
+for i in arr:
+    ans += abs(i - target)
+
+print(ans)    
