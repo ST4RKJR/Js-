eb43e2081d0cf6ccc78e53d472c26348e3f6cc22
Stark <sahil.gen9@gmail.com>
2025-05-16 18:37:16 +0530

    100DaysOfCode

--- 100DaysOfCode/Day94.py
+++ 100DaysOfCode/Day94.py
@@ -20,3 +20,29 @@ while True:
         print(k - n)
         exit()
     k += 2
+    
+    
+## CLOSEST PRIME NUMBERS IN RANGE
+def closestPrimes(left, right):
+    
+    primes = [1] * (right + 1)
+    primes[0] = primes[1] = 0
+    p = 2
+    while p * p <= right:
+        if primes[p]:
+            for i in range(p * p, right + 1, p):
+                primes[i] = 0
+        p += 1
+    
+    prr = [i for i in range(left, right + 1) if primes[i]]
+    min_len = float("inf")
+    ans = [-1, -1]
+
+    if len(prr) < 2:
+        return ans
+
+    for i in range(len(prr) - 1):
+        if min_len > prr[i + 1] - prr[i]:
+            ans = [prr[i], prr[i + 1]]
+            min_len = prr[i + 1] - prr[i]
+    return ans
