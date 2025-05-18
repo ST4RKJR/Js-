46708eabf9d1a0e9159c0c7125d26a4f075871c2
Stark <sahil.gen9@gmail.com>
2025-05-18 18:39:08 +0530

    100DaysOfCode

---
+++ 100DaysOfCode/Day96.py
@@ -0,0 +1,42 @@
+## LONGEST INCREASING SUBSEQUENCE
+def lengthOfLIS(nums):
+    # memo = {}
+
+    # def f(i):
+    #     if i in memo:
+    #         return memo[i]
+
+    #     if i == 0:
+    #         return 1
+
+    #     maxi = 1
+    #     for j in range(i):
+    #         if nums[j] < nums[i]:
+    #             maxi = max(maxi, f(j) + 1)
+    #     memo[i] = maxi 
+    #     return memo[i]
+
+    # ans = 1
+    # for i in range(1, len(nums)):
+    #     ans = max(ans, f(i))
+    # return ans
+
+    memo = {}
+    def f(i):
+        if i in memo:
+            return memo[i]
+        
+        if i == 0:
+            return 1
+        
+        maxi = 1
+        for j in range(i):
+            if nums[j] < nums[i]:
+                maxi = max(maxi, f(j) + 1)
+        memo[i] = maxi
+        return memo[i]
+
+    ans = 1
+    for i in range(1, len(nums)):
+        ans = max(ans, f(i))
+    return ans
