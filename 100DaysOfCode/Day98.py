5af14265f0d8301fd1d50cc18c1828cf7b159c11
Stark <sahil.gen9@gmail.com>
2025-05-20 18:41:27 +0530

    100DaysOfCode

--- 100DaysOfCode/Day98.py
+++ 100DaysOfCode/Day98.py
@@ -11,3 +11,25 @@ for i in range(n, 2 * n):
     second += int(s[i])
 
 print("LUCKY" if first == second else "UNLUCKY")
+
+## ROmAN NUMBER TO INTEGER
+
+def romanToInt(s):
+    d = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+    
+    ans = 0
+    for i in range(len(s)):
+        if i + 1 < len(s) and d[s[i]] < d[s[i + 1]]:
+            ans -= d[s[i]]
+        else:
+            ans += d[s[i]]
+        
+    return ans
