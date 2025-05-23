b13cf94e18b0d73f5f789d14122c4f741977d04a
Stark <sahil.gen9@gmail.com>
2025-05-23 18:49:09 +0530

    Day78.py

--- 100DaysOfCode/Day78.py
+++ 100DaysOfCode/Day78.py
@@ -4,3 +4,29 @@ def odd_sum(arr):
     for i, val in enumerate(arr):
         ans += val if i % 2 else 0
     return ans
+
+## BEST TIME TO BUY AND SELL STOCK III
+def maxProfit(prices):
+    n = len(prices)
+    # prefix = [0] * (n)
+    # prefix[0] = prices[0]
+    # for i in range(1, n):
+    #     prefix[i] = min(prefix[i - 1], prices[i])
+    
+    # suffix = [0] * (n)
+    # suffix[n - 1] = prices[n - 1]
+    # for i in range(n - 2, -1, -1):
+    #     suffix[i] = max(suffix[i + 1], prices[i])
+    
+    # ans = 0
+    # for i in range(n):
+    #     ans = max(ans, suffix[i] - prefix[i])
+    # return ans
+
+
+    mini = prices[0]
+    ans = 0
+    for i in prices:
+        mini = min(mini, i)
+        ans = max(ans, i - mini)
+    return ans
