feb7950562e0902627d45f657d4fc4d1039cf3e5
Stark <sahil.gen9@gmail.com>
2025-05-24 18:51:35 +0530

    Day79.py

--- 100DaysOfCode/Day79.py
+++ 100DaysOfCode/Day79.py
@@ -33,3 +33,40 @@ def coin_change_minimum():
     print('\n'.join(results))
 
 coin_change_minimum()
+
+
+## SUM OF EVEN 
+
+class Node:
+    def __init__(self, val=1):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+from collections import deque
+
+def build_tree(level_order):
+    if not level_order or level_order[0] == 0:
+        return None
+
+    root = Node(level_order[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(level_order):
+        current = queue.popleft()
+        
+        # Left child
+        if i < len(level_order) and level_order[i] != 0:
+            current.left = Node(level_order[i])
+            queue.append(current.left)
+        i += 1
+
+        # Right child
+        if i < len(level_order) and level_order[i] != 0:
+            current.right = Node(level_order[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
